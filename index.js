d970700554afba6e571fd2fe7d66f38826b96da1
Daryl Norrell <cheyannewascom@gmail.com>
2022-07-24 17:56:53 -0700

    commit tree eaf57a551b2e515a038c78de126534038c7d3c60

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    let i = 0;
+    while (i < gifts.length) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        i++;
+    }
+
+    return gifts;
+}
+
+const names = [];
+function writeCards(names) {
+    const thanks = [];
+    for (let i = 0; i < names.length; i++){
+        thanks.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    console.log(thanks);
+    return thanks;
+}
+
+function countDown(number) {
+    number;
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+
+    return number;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+wrapGifts(gifts);
+countDown(10);
